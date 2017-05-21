8525561217eace2a7edb9f8e89b7bdfd10d7b37b
Lalit Kishor <lalit9799991992@gmail.com>
2017-05-21 15:10:08 +0530

    google ad added

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		  (adsbygoogle = window.adsbygoogle || []).push({
+		    google_ad_client: "ca-pub-4365975382552630",
+		    enable_page_level_ads: true
+		  });
+		</script>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Lalit Sharma - Salesforce Designer & Front End Designer</title>
